69f778e8858defe778f60a9e8807fe26a796e7a2
carsenio25 <112511512+carsenio25@users.noreply.github.com>
2024-05-20 16:34:24 -0400

    Prep main.jsx for merge

--- TJElementary/src/main.jsx
+++ TJElementary/src/main.jsx
@@ -7,18 +7,38 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Layout from "./Layout";
 import { Root } from "./roots/Root.jsx";
+import { Calander } from "./roots/Calander.jsx";
 import { TeacherDirectory } from "./roots/TeacherDirectory.jsx";
+import { StudentDirectory } from "./roots/StudentDirectory.jsx";
+import { Grades } from "./roots/Grades.jsx";
+import { Dashboard } from "./roots/Dashboard.jsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Layout><Root /></Layout>,
 	  },
+  {
+    path: "/calander",
+    element: <Layout><Calander /></Layout>,
+  },
+  {
+    path: "/teacherdirectory",
+    element: <Layout><TeacherDirectory /></Layout>,
+  },
+  {
+      path: "/studentDirectory",
+      element: <Layout><StudentDirectory /></Layout>,
+  },
+  {
+      path: "/grades",
+      element: <Layout><Grades /></Layout>,
+  },
+  {
+      path: "/courseDashboard",
+      element: <Layout><Dashboard /></Layout>,
+  },
 
-	{
-		path: "/teachers",
-		element: <Layout><TeacherDirectory /></Layout>,
-	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
