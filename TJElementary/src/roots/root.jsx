234f320fef2c93e2b420a03e10c2ec01abb582bf
Anna Batman <annadbatman@gmail.com>
2024-05-20 16:18:20 -0400

    set up routes

---
+++ TJElementary/src/roots/root.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import picture from "./BrazilPic.jpg";
+
+export const Root = () => {
+    return (
+        <div>
+            <h1>TJ Elementary</h1>
+        </div>
+    );
+};
