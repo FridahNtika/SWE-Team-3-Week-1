bf1cf718d6db8dd6f7e7b2e328ad56444af32772
Anna Batman <annadbatman@gmail.com>
2024-05-20 16:19:25 -0400

    Routes

--- TJElementary/src/App.jsx
+++ TJElementary/src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -8,7 +9,7 @@ function App() {
 
     return (
         <>
-            <h1>My App</h1>
+            <h1>TJ Elementary</h1>
         </>
     );
 }
--- TJElementary/src/main.jsx
+++ TJElementary/src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Root } from "./roots/Root.jsx";
+import { Calander } from "./roots/Calander.jsx";
+import { TeacherDirectory } from "./roots/TeacherDirectory.jsx";
+import { StudentDirectory } from "./roots/StudentDirectory.jsx";
+import { Grades } from "./roots/Grades.jsx";
+import { Dashboard } from "./roots/Dashboard.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+    },
+    {
+        path: "/calander",
+        element: <Calander />,
+    },
+    {
+        path: "/teacherdirectory",
+        element: <TeacherDirectory />,
+    },
+    {
+        path: "/studentDirectory",
+        element: <StudentDirectory />,
+    },
+    {
+        path: "/grades",
+        element: <Grades />,
+    },
+    {
+        path: "/courseDashboard",
+        element: <Dashboard />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
