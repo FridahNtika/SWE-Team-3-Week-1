cf6c1e8bc39f04c11cafc07fc7a0118a83c77e8d
carsenio25 <112511512+carsenio25@users.noreply.github.com>
2024-05-21 14:50:05 -0400

    fix privacy error

--- TJElementary/firebase.js
+++ TJElementary/firebase.js
@@ -7,19 +7,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    // apiKey: import.meta.env.VITE_API_KEY,
-    // authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    // projectId: import.meta.env.VITE_PROJECT_ID,
-    // storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    // messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    // appId: import.meta.env.VITE_APP_ID,
-    apiKey: "AIzaSyDeZ0DHam_eREnZXiMD7OsIHDT2w_gd7LE",
-  authDomain: "swe-team-3-week-1.firebaseapp.com",
-  projectId: "swe-team-3-week-1",
-  storageBucket: "swe-team-3-week-1.appspot.com",
-  messagingSenderId: "748121290174",
-  appId: "1:748121290174:web:b93a100b116270945435ef",
-  measurementId: "G-3ZTD3CGB2F"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
