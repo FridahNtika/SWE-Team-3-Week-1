ed3ffa4e286afc46a9c703449f83c3ffd1e29b27
FridahNtika <fn101@wellesley.edu>
2024-05-21 16:09:15 -0400

    course dashboard updates

--- TJElementary/src/roots/Dashboard.jsx
+++ TJElementary/src/roots/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from "react";
-//import {doc, getDoc} from 'firebase/firestore';
-//import { db } from './firebase.js';
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 export const Dashboard = () => {
     // get the courses from the database
@@ -10,9 +10,30 @@ export const Dashboard = () => {
     2. title 
     3. teacher */
     // have a section to filter the courses**
+    const [courses, setCourses] = useState([]);
+    const [error, setError] = useState(null);
+
+    const getCourses = async () => {
+        try {
+            const allDocs = await getDocs(collection(db, 'classes'));
+            let courseArray = [];
+            //allDocs.forEach((doc) => console.log(doc.data()))
+            allDocs.forEach((doc) => 
+                courseArray.push(doc.data()));
+            setCourses(courseArray);
+            console.log(courses);
+        } catch (error) {
+            setError(error.message);
+        };
+    }
+
+    useEffect(() => {
+        getCourses();
+        }, []);
 
     return (
     <>
+    <h2>Course Browser</h2>
     <div id="courseListing">
         <div>
             Listing for: 
@@ -21,7 +42,17 @@ export const Dashboard = () => {
             <b> 56*</b>
         </div>
         {/* iterate through each course here */}
-
+        {courses.map((course) => (
+            <section>
+                <div key={course}>
+                    <p>{course[0].title}</p>
+                    <p>{course.descr}</p>
+                    <p>{course.teacher}</p>
+                    <p>Current enrollment: {course.current}</p>
+                    <p>Max enrollment: {course.max}</p>
+                </div>
+            </section>
+        ))}
     </div>
     </>
     );
