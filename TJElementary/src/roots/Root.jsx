63f75c84bb994ed1de7cc40045638c04b5e0e173
carsenio25 <112511512+carsenio25@users.noreply.github.com>
2024-05-21 16:29:33 -0400

    Merge fix

--- TJElementary/src/roots/Root.jsx
+++ TJElementary/src/roots/Root.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Root = () => {
-	return (
-		<div>
-			<h1>Root Page</h1>
+    return (
+        <>
+            <h1>Welcome to TJ Elementary</h1>
             <Link to={"/teachers"}>Go to teacher directory page</Link>
-		</div>
-        
-	);
+            <br />
+            <Link to={"/teachers"}>Go to teacher directory page</Link>
+        </>
+    );
 };
