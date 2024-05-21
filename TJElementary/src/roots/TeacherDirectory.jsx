881e87fbb29036d3016ec4ce8ccda253a6d77169
carsenio25 <112511512+carsenio25@users.noreply.github.com>
2024-05-21 15:10:41 -0400

    add inputting a teacher to directory

--- TJElementary/src/roots/TeacherDirectory.jsx
+++ TJElementary/src/roots/TeacherDirectory.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 import { useState, useEffect } from "react";
 // import "./App.css";
 import { db } from "../../firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc, updateDoc, doc } from "firebase/firestore";
+
 
 export const TeacherDirectory = () => {
+    const [teacherName, setTeacherName] = useState("");
+    const [teacherEmail, setTeacherEmail] = useState("");
+    const [teacherSubject, setTeacherSubject] = useState("");
+
+
     const [teachers, setTeachers] = useState([]);
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const docRef = await addDoc(collection(db, "teachers"), {
+                id: 123456,
+                email: teacherName + "@jefferson.edu",
+                name: teacherName,
+                subject: teacherSubject
+            });
+            console.log("Created doc with ID: ", docRef.id);
+            setTeacherName("");
+            setTeacherSubject("");
+            fetchTeachers();
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    };
+
 
     const fetchTeachers = async () => {
         try {
@@ -22,13 +49,30 @@ export const TeacherDirectory = () => {
         }
     };
 
+
     useEffect(() => {
         fetchTeachers();
     }, []);
 
+
     return (
         <div>
             <h1>Teacher Directory</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Add a teacher:</label>
+                <input
+                    type="text"
+                    value={teacherName}
+                    onChange={(e) => setTeacherName(e.target.value)}
+                ></input>
+                <input
+                    type="text"
+                    value={teacherSubject}
+                    onChange={(e) => setTeacherSubject(e.target.value)}
+                ></input>
+                <br></br>
+                <button type="submit">Submit</button>
+            </form>
             <table>
                 <thead>
                     <tr>
