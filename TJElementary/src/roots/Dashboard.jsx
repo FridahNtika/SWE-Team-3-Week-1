58b794b99ae52f81ddc51aef9b3c511da26a7526
FridahNtika <fn101@wellesley.edu>
2024-05-22 10:57:12 -0400

    print out the courses offered

--- TJElementary/src/roots/Dashboard.jsx
+++ TJElementary/src/roots/Dashboard.jsx
@@ -4,22 +4,25 @@ import { db } from "../../firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 export const Dashboard = () => {
-    /* iterate through each course printing out the:
-    1. current enrolment
-    2. title 
-    3. teacher */
     // have a section to filter the courses**
     const [courses, setCourses] = useState([]);
+    const [total, setTotal] = useState(0);
     const [error, setError] = useState(null);
 
     const fetchCourses = async () => {
         try {
             const querySnapshot = await getDocs(collection(db, "classes"));
-            let coursesArray = [];
+            let temp = [];
             querySnapshot.forEach((doc) => {
-                coursesArray.push({ id: doc.id, ...doc.data() });
+                temp.push({ id: doc.id, ...doc.data() });
             });
-            console.log(coursesArray);
+            let coursesArray = [];
+
+            temp.forEach((item) => {
+                coursesArray.push(item);
+            });
+            const total = coursesArray.length;
+            setTotal(total);
             setCourses(coursesArray);
         } catch (error) {
             console.error("Error fetching courses: ", error);
@@ -32,13 +35,28 @@ export const Dashboard = () => {
 
     return (
     <>
-    <h2>Course Browser</h2>
+    <div className='header'>
+        <a class="school-title" href="/"> Thomas Jefferson Elementary</a><br></br>
+        <a class="courses" href="/courseDashboard"> Course Browser</a>
+    </div>
+    <hr />
     <div id="courseListing">
         <div>
             Listing for: 
             <b> Fall 2024</b>
             ; Total courses offered: 
-            <b> 56*</b>
+            <b> {total} </b>
+            <hr />
+        </div>
+        <div>
+            {courses.map((course) => (
+                <section>
+                    <p>{course.Title} ({course['Course Code']}) by {course.Teacher}</p>
+                    <p>{course.Description}</p>
+                    <p>{course['Current Enrollment']} out of {course['Max Enrollment']} currently enrolled</p>
+                    <hr />
+                </section>
+            ))}
         </div>
     </div>
     </>
