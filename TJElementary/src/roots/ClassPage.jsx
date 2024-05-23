28c52dcbf721a598b053d07622f4a984d7ed878f
FridahNtika <fn101@wellesley.edu>
2024-05-23 14:33:40 -0400

    displays page specific information

--- TJElementary/src/roots/ClassPage.jsx
+++ TJElementary/src/roots/ClassPage.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { db } from "../../firebase";
 import { getDoc, updateDoc, doc, deleteDoc } from "firebase/firestore";
 import { Dashboard } from './Dashboard';
+import { TextField, Button, Card, CardContent, Typography, Container, Grid } from "@mui/material";
 
 export const ClassPage = () => {
     const [courseInfo, setCourseInfo] = useState(null);
@@ -11,6 +12,7 @@ export const ClassPage = () => {
     // Get the userId param from the URL.
     let { id } = useParams();
     const properID = id.replace(':','');
+    //console.log(properID);
 
     // Gets the course's data
     useEffect(() => {
@@ -19,7 +21,10 @@ export const ClassPage = () => {
                 const docRef = await getDoc(doc(db, "classes", properID));
                 const data = docRef.data();
                 setCourseInfo({ data });
-                const stud = courseInfo['data'].Enrolled;
+                //console.log(data);
+                console.log("CI: ", courseInfo);
+                const stud = []
+                courseInfo['data'].Enrolled.forEach((student) => stud.push(student));
                 setStudents(stud);
             } catch (error) {
                 console.log("Error fetching this course: ", error);     
@@ -35,13 +40,23 @@ export const ClassPage = () => {
     // Allows administrators to update the existing class
     const handleUpdate = async (evt) => {
         evt.preventDefault();
-        try {
+        try {0
             const docRef = await doc(db, "classes", properID);
             await updateDoc(docRef(db, "classes", properID), {
                 property: newValue
               });
         } catch (error) {
             console.log("Error updating this course: ", error);
+        }
+    };
+
+    const handleDelete = async (idx) => {
+        try {
+            //console.log(students);
+            students.pop(idx);
+            //console.log(students);
+        } catch (error) {
+            console.error("Error removing student from course: ", error);
         }
     };
         
@@ -55,26 +70,46 @@ export const ClassPage = () => {
         </Route>
         </Routes>*/}
         <section>
+            <>
             <div className='courseInfo'>
                 {courseInfo ? (
                     <>
                     <h2>{courseInfo['data'].Title} ({courseInfo['data']['Course Code']})</h2>
+                    <p><strong>Meets on {courseInfo['data']['Meeting Times']}</strong></p>
                     <p><i>{courseInfo['data'].Teacher}</i></p>
                     <p>{courseInfo['data'].Description}</p>
                     <h4>Students currently enrolled: </h4>
-                    <div className='studentList'>
-                        {students.map((student, index) => (
-                            <div key={index}>
-                                <p>{student}</p> 
+                    <Container>
+                        <Grid container spacing={2} marginTop={2}>
+                            <div className='studentList'>
+                                {students ? (
+                                    students.map((student, index) => (
+                                    <Grid item xs={12} sm={6} md={4} key={student.id}>
+                                        <Card>
+                                            <CardContent>
+                                                <>
+                                                <Typography variant="h5">{student}</Typography>
+                                                <Button variant="contained" color="secondary" onClick={() => handleDelete(index)} 
+                                                    sx={{ backgroundColor: 'teal', '&:hover': { backgroundColor: '#008080' }}}>
+                                                    Remove </Button>
+                                                </>
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                    ))
+                                ) : (
+                                    <p>No students enrolled in this course</p>
+                                )} 
                             </div>
-                            ))
-                        }
-                    </div>
+                        </Grid>
+                    </Container>
                     </>
                 ) : (
                     <p>Loading...</p>
-                )}
-        </div>
+                )
+                }
+            </div>
+            </>
         </section>
         {/*<section>
         <div className='updateEnrollment'>
